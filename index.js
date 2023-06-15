37540f72dbea8d3a60924a4a68db0e6f12384864
Joy Olago <joydanielle62@gmail.com>
2023-06-16 00:10:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let rideInFeet
+
+function scuberGreetingForFeet(rideInFeet) {
+  if (rideInFeet <= 199) {
+  return 'This one is on me!';
+} else if (rideInFeet > 2000 && rideInFeet < 2500 ) {
+  return 'I will gladly take your thirty bucks.';
+} else if (rideInFeet >2500){
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city = 'NYC'
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+  return ('Ok, sounds good.');
+} else if (city) {
+  return 'No go.'
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip = 'generous' 
+
+function switchOnCharmFromTip(tip){
+ if (tip === 'generous') {
+  return 'Thank you so much.'
+ } else if (tip === 'not as generous' ) {
+  return 'Thank you.'
+ } else {
+  return 'Bye.'
+ }
 }
